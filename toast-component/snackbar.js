85eae5ad0420b7d9fa5c3723b692d5fda388d821
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-02-21 09:24:38 +0530

    Update snackbar.js

--- toast-component/snackbar.js
+++ toast-component/snackbar.js
@@ -1,7 +1,10 @@
-const snackbarCloseButtons = document.querySelectorAll('.btn-snackbar-close');
-snackbarCloseButtons.forEach(closeButtons => closeButtons.addEventListener('click', closeSnackbar));
+/* Reference to the snackbar close button */
+const snackbarCloseButton = document.querySelector('.btn-snackbar-close');
 
-// Function to close snackbar when close button is clicked
+/* Attaching event listener to listen to click event on the snackbar close button */
+snackbarCloseButton.addEventListener('click', closeSnackbar);
+
+/* Even handler to close the snackbar when close button is clicked */
 function closeSnackbar(e) {
     e.preventDefault();
     e.target.parentElement.parentElement.style.display = 'none';
