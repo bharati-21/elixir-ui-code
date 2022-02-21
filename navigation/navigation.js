fb73d207aaf4c0f61bd195e205908f4ced47ba69
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-02-21 11:29:33 +0530

    Update navigation.js

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,11 +1,17 @@
+/* Reference to the navigatio bar */
 const navigation = document.querySelector('.navigation-bar');
+
+/* Reference to the navigatio bar hamburger icon */
 const navigationHamburger = document.querySelector('.navigation-bar-hamburger');
+
+/* Reference to the navigatio bar close icon*/
 const navigationCloseButton = document.querySelector('.btn-navigation-close');
 
+/* Attaching listeners on hamburger and navigation close buttons to listen for click event */
 navigationHamburger.addEventListener('click', openNavigation);
 navigationCloseButton.addEventListener('click', openNavigation);
 
-
+/* Handler that toggles navigation bar open status */
 function openNavigation(e) {
     navigation.classList.toggle('navigation-open');
 }
